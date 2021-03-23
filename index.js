49566995c7bfb6fa6c4cd55efbb972fdf98e8e76
AJ Auntor <auntorknocks@gmail.com>
2021-03-24 00:42:15 +0600

    Call data

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require ('express');
+const express = require('express');
 
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('This is respond form Backend')
+    const phone = {
+        price: 100,
+        name: 'samsung'
+    }
+    res.send(phone);
 });
+app.get('/phone/samsung', (req, res) => {
+    res.send({ name: 'oppo', price: 451, stock: 17 });
+})
 
-app.listen(3005, () => console.log('Database calling from 3005 port!') );
+app.listen(3005, () => console.log('Database calling from 3005 port!'));
