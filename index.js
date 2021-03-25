9e2a17e14b6252154c43013a97e7b6e5d7ad4902
AJ Auntor <auntorknocks@gmail.com>
2021-03-25 12:04:14 +0600

    Read Dynamic Data From Api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 
 const app = express();
+const users = ["Auntor", "Sajal", "Amdadul", "Rakib", "Dabid Carry", "Chinlo Hamsk", "Jack Naim", "Liza"];
 
 app.get('/', (req, res) => {
     const phone = {
@@ -12,6 +13,12 @@ app.get('/', (req, res) => {
 });
 app.get('/phone/samsung', (req, res) => {
     res.send({ name: 'oppo', price: 451, stock: 17 });
+})
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const name = users[userId];
+    res.send({userId, name});
+    
 })
 
 app.listen(3005, () => console.log('Database calling from 3005 port!'));
