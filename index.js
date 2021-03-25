b16616e490996c239db96b7cc0313d0797b49660
AJ Auntor <auntorknocks@gmail.com>
2021-03-25 13:03:30 +0600

    Reslovd cors policy issus

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const express = require('express');
+var cors = require('cors')
 
 
 const app = express();
-const users = ["Auntor", "Sajal", "Amdadul", "Rakib", "Dabid Carry", "Chinlo Hamsk", "Jack Naim", "Liza"];
+app.use(cors())
 
-app.get('/', (req, res) => {
-    const phone = {
-        price: 100,
-        name: 'samsung'
-    }
-    res.send(phone);
-});
-app.get('/phone/samsung', (req, res) => {
-    res.send({ name: 'oppo', price: 451, stock: 17 });
-})
+const users = ["Auntor", "Sajal", "Amdadul", "Rakib", "Imran", "Hasib"];
+
 app.get('/users/:id', (req, res) => {
     const userId = req.params.id;
     const name = users[userId];
@@ -22,3 +15,27 @@ app.get('/users/:id', (req, res) => {
 })
 
 app.listen(3005, () => console.log('Database calling from 3005 port!'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/phone/samsung', (req, res) => {
+//     res.send({ name: 'oppo', price: 451, stock: 17 });
+// })
+
+// app.get('/', (req, res) => {
+//     const phone = {
+//         price: 100,
+//         name: 'samsung'
+//     }
+//     res.send(phone);
+// });
